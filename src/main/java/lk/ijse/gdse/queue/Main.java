ba486063c8fa5b9092a20d54f3def83e876a7c3d
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-22 13:43:39 +0530

    queue isFull() , grow()

--- src/main/java/lk/ijse/gdse/queue/Main.java
+++ src/main/java/lk/ijse/gdse/queue/Main.java
@@ -6,10 +6,21 @@ public class Main {
         queue.enQue(10);
         queue.enQue(20);
         queue.enQue(30);
+        queue.enQue(40);
+        queue.enQue(50);
+        queue.enQue(60);
+        queue.enQue(70);
+        queue.enQue(80);
+        queue.enQue(90);
 
+        queue.deQueue();  //[10,20,30] --> mulinma thiyena eka remove wenawa --> 10 remove
         queue.deQueue();
+        System.out.println(queue.peek());    // dn mulinm THIYENA EKA PRINT WENAWA ---> 20 print
 
-        System.out.println(queue.peek());
+        queue.printQueue();
+
+        System.out.println("Size : " + queue.size());
+
 
     }
 }
--- src/main/java/lk/ijse/gdse/queue/Queue.java
+++ src/main/java/lk/ijse/gdse/queue/Queue.java
@@ -12,6 +12,10 @@ public class Queue {
     }
 
     public void enQue(int data){
+        if (isFull()){
+            grow();
+        }
+
         if (front == -1){
             front = 0;
         }
@@ -25,5 +29,30 @@ public class Queue {
 
     public int peek(){
         return elementData[front];
+    }
+
+    public int size(){
+        return rear - front + 1;
+    }
+
+    public void printQueue(){
+        System.out.print("[ ");
+        for (int i = front; i <= rear ; i++) {
+            System.out.print(elementData[i] + "  ");
+        }
+        System.out.print("]");
+    }
+
+    public boolean isFull(){
+        return rear == elementData.length - 1;
+    }
+
+    private void grow(){
+        int[] temp = elementData;
+
+        elementData = new int[elementData.length * 2];
+        for (int i = 0; i < temp.length; i++) {
+            elementData[i] = temp[i];
+        }
     }
 }
