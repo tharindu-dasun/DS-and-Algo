6e394038695e22906d36ff3c0f2fcfbe3223609e
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-27 23:17:55 +0530

    update 0.2

--- src/main/java/lk/ijse/gdse/linkedlist/ArrayList.java
+++ src/main/java/lk/ijse/gdse/linkedlist/ArrayList.java
@@ -1,4 +1,13 @@
 package lk.ijse.gdse.linkedlist;
-
 public class ArrayList {
+    public static void main(String[] args) {
+//        ArrayList<String> cars = new ArrayList<String>();
+//        cars.add("Volvo");
+//        cars.add("BMW");
+//        cars.add("Ford");
+//        cars.add("Mazda");
+//        System.out.println(cars);
+//    }
+
+    }
 }
